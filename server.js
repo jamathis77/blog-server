dcf1e90c3544fff178262bad0633241fc433a5a1
Jacob Mathis <jamathis77@gmail.com>
2018-01-05 16:10:49 -0500

    added delete and post

--- server.js
+++ server.js
@@ -27,6 +27,38 @@ app.post('/blog-posts', jsonParser, (req, res) => {
   }
   const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
   res.status(201).json(item);
+});
+
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`
+    console.error(message)
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
 })
 
 app.listen(process.env.PORT || 8080, () => {
